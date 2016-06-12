de3621fca6bc9e97adad47ecd2bef90208ac622f
Murray Smith <murray@mismith.info>
2016-06-11 20:02:18 -0600

    trim/improve data + processing

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -4,7 +4,8 @@ var MiResume = Vue.extend({
 	data: function data() {
 		return {
 			resume: null,
-			isActive: []
+			filtered: null,
+			actives: []
 		};
 	},
 
@@ -20,12 +21,18 @@ var MiResume = Vue.extend({
 		},
 		sortByName: function sortByName(items) {
 			return items.sort(function (a, b) {
-				return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1;
+				var A = a.name.toLowerCase(),
+				    B = b.name.toLowerCase();
+
+				return A < B ? -1 : B < A ? 1 : 0;
 			});
 		},
 		sortByPriority: function sortByPriority(items) {
 			return items.sort(function (a, b) {
-				return (a.priority || items.length) < (b.priority || items.length) ? -1 : 1;
+				var A = a.priority || items.length,
+				    B = b.priority || items.length;
+
+				return A < B ? -1 : B < A ? 1 : 0;
 			});
 		},
 		topPicks: function topPicks(items) {
@@ -45,11 +52,14 @@ var MiResume = Vue.extend({
 				return replacements[key];
 			});
 		},
-		getActive: function getActive(item) {
-			return this.isActive.indexOf(item) >= 0;
+		className: function className(name) {
+			return name.toLowerCase().replace(' ', '-').replace('.', '');
+		},
+		isActive: function isActive(item) {
+			return this.actives.indexOf(item) >= 0;
 		},
 		toggleActive: function toggleActive(item) {
-			return this.getActive(item) ? this.isActive.splice(this.isActive.indexOf(item), 1) : this.isActive.push(item);
+			return this.isActive(item) ? this.actives.splice(this.actives.indexOf(item), 1) : this.actives.push(item);
 		}
 	},
 	init: function init() {
@@ -58,11 +68,38 @@ var MiResume = Vue.extend({
 		fetch('resume.json').then(function (res) {
 			return res.json();
 		}).then(function (json) {
-			return _this.resume = json;
+			_this.resume = json;
+			_this.filtered = {};
+
+			for (var section in _this.resume) {
+				switch (section) {
+					case 'experience':
+					case 'education':
+					case 'volunteering':
+						// remove irrelevant entries
+						_this.filtered[section] = _this.resume[section].filter(function (item) {
+							return item.relevant !== false;
+						});
+						break;
+					case 'about':
+						// flip order for columnal display
+						_this.filtered[section] = [];
+						var half = Math.ceil(_this.resume[section].length / 2);
+						for (var i = 0; i < _this.resume[section].length; i++) {
+							var j = i % 2 === 0 ? i / 2 : half + Math.floor(i / 2);
+							_this.filtered[section][i] = _this.resume[section][j];
+						}
+						break;
+					default:
+						// no filtering needed
+						_this.filtered[section] = _this.resume[section];
+						break;
+				}
+			}
 		});
 	},
 
-	template: '\n<section>\n\t<header>\n\t\t<img :src="resume.info.image" />\n\t\t<h1>{{ resume.info.name }}</h1>\n\t\t<p>{{ resume.info.description }}</p>\n\t</header>\n\t<article class="about">\n\t\t<header>\n\t\t\t<h2>about</h2>\n\t\t</header>\n\t\t<ul>\n\t\t\t<li v-for="skill in resume.skills">\n\t\t\t\t<strong>{{ skill.name }}</strong>:\n\t\t\t\t<span>{{ skill.description + (skill.items ? topPicks(skill.items) + \'.\' : \'\') }}</span>\n\t\t\t\t<a v-if="skill.items" href @click.prevent="toggleActive(skill)">Show {{ getActive(skill) ? \'Less\' : \'More\' }}</a>\n\t\t\t\t<p v-if="skill.items" v-show="getActive(skill)">{{ listOfNames(skill.items) }}</p>\n\t\t\t</li>\n\t\t</ul>\n\t</article>\n\t<article v-for="section of [\'social\',\'experience\',\'education\',\'volunteering\']" :class="section">\n\t\t<header>\n\t\t\t<h2>{{ section }}</h2>\n\t\t</header>\n\t\t<ol>\n\t\t\t<li v-for="item of resume[section]">\n\t\t\t\t<header>\n\t\t\t\t\t<span v-if="item.title"><strong>{{ item.title }}</strong>,</span>\n\t\t\t\t\t<a v-if="item.url" :href="item.url" target="_blank">{{ item.name }}</a>\n\t\t\t\t\t<small v-if="item.duration">{{ item.duration }}</small>\n\t\t\t\t</header>\n\t\t\t\t<div v-if="item.description">{{ item.description }}</div>\n\t\t\t</li>\n\t\t</ol>\n\t</article>\n</section>'
+	template: '\n<div>\n\t<header class="flex-row flex-align-center">\n\t\t<div class="flex-grow flex-shrink padding-right" style="flex-basis: 50%;">\n\t\t\t<h1>{{ resume.info.name }}</h1>\n\t\t\t<p>{{ resume.info.description }}</p>\n\t\t</div>\n\t\t<figure>\n\t\t\t<img :src="resume.info.image" width="128" height="128" />\n\t\t</figure>\n\t</header>\n\t<article class="about">\n\t\t<header>\n\t\t\t<h2>about</h2>\n\t\t</header>\n\t\t<ul class="flex-row">\n\t\t\t<li v-for="skill of filtered.about" class="iconed">\n\t\t\t\t<i class="fa fa-{{ skill.icon }}" :title="skill.name"></i>\n\t\t\t\t<span>{{ skill.description + (skill.items ? topPicks(skill.items) + \'.\' : \'\') }}</span>\n\t\t\t\t<button v-if="skill.items" @click="toggleActive(skill)" class="btn more"><i class="fa fa-{{ isActive(skill) ? \'minus\' : \'plus\' }}-circle"></i> {{ skill.items.length - 3 }} {{ isActive(skill) ? \'less\' : \'more\' }}</button>\n\t\t\t\t<ul v-if="skill.items" v-show="isActive(skill)" class="more">\n\t\t\t\t\t<li v-for="item of skill.items">{{ item.name }}<sup v-if="item.priority" class="color-primary">&star;</sup></li>\n\t\t\t\t</ul>\n\t\t\t</li>\n\t\t</ul>\n\t</article>\n\t<article class="social">\n\t\t<header>\n\t\t\t<h2>social</h2>\n\t\t</header>\n\t\t<ul class="flex-row flex-justify-around">\n\t\t\t<li v-for="item of resume.social">\n\t\t\t\t<a :href="item.url" :title="item.name" target="_blank" :class="\'fa fa-\' + className(item.name)"></a>\n\t\t\t</li>\n\t\t</ul>\n\t</article>\n\t<article v-for="section of [\'experience\',\'education\',\'volunteering\']">\n\t\t<header>\n\t\t\t<h2>{{ section }}</h2>\n\t\t</header>\n\t\t<ol>\n\t\t\t<li v-for="item of (isActive(section) ? resume : filtered)[section]">\n\t\t\t\t<header>\n\t\t\t\t\t<strong v-if="item.title">{{ item.title }}</strong>\n\t\t\t\t\t<a v-if="item.url" :href="item.url" target="_blank">{{ item.name }}</a>\n\t\t\t\t\t<small v-if="item.duration">{{ item.duration }}</small>\n\t\t\t\t</header>\n\t\t\t\t<div v-if="item.description">{{ item.description }}</div>\n\t\t\t</li>\n\t\t</ol>\n\t\t<footer>\n\t\t\t<button v-if="filtered[section].length < resume[section].length" @click="toggleActive(section)" class="btn more"><i class="fa fa-{{ isActive(section) ? \'minus\' : \'plus\' }}-circle"></i> {{ resume[section].length - filtered[section].length }} {{ isActive(section) ? \'less\' : \'more\' }}</button>\n\t\t</footer>\n\t</article>\n\t<article>\n\t\t<header>\n\t\t\t<h2>references</h2>\n\t\t</header>\n\t\t<ul>\n\t\t\t<li>\n\t\t\t\tAvailable on <a :href="\'mailto:\' + resume.info.email" target="_blank">request</a>.\n\t\t\t</li>\n\t\t</ul>\n\t</article>\n</div>'
 });
 
 var MiApp = new Vue({
@@ -70,5 +107,5 @@ var MiApp = new Vue({
 	components: {
 		MiResume: MiResume
 	},
-	template: '<mi-resume></mi-resume>'
+	template: '\n<main>\n\t<mi-resume></mi-resume>\n</main>'
 });
--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -2,7 +2,8 @@ let MiResume = Vue.extend({
 	data() {
 		return {
 			resume: null,
-			isActive: [],
+			filtered: null,
+			actives: [],
 		};
 	},
 	methods: {
@@ -14,10 +15,20 @@ let MiResume = Vue.extend({
 			return names.map(item => item.name).join(', ');
 		},
 		sortByName(items) {
-			return items.sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1);
+			return items.sort((a, b) => {
+				let A = a.name.toLowerCase(),
+					B = b.name.toLowerCase();
+
+				return A < B ? -1 : (B < A ? 1 : 0);
+			});
 		},
 		sortByPriority(items) {
-			return items.sort((a, b) => (a.priority || items.length) < (b.priority || items.length) ? -1 : 1);
+			return items.sort((a, b) => {
+				let A = a.priority || items.length,
+					B = b.priority || items.length;
+
+				return A < B ? -1 : (B < A ? 1 : 0);
+			});
 		},
 		topPicks(items, num = 3) {
 			if (!items) return;
@@ -32,55 +43,114 @@ let MiResume = Vue.extend({
 			let regex = new RegExp(`\\$\\{(${Object.keys(replacements).join('|')})\\}`, 'ig');
 			return text.replace(regex, (m, key) => replacements[key]);
 		},
+		className(name) {
+			return name.toLowerCase().replace(' ', '-').replace('.', '');
+		},
 
-		getActive(item) {
-			return this.isActive.indexOf(item) >= 0;
+		isActive(item) {
+			return this.actives.indexOf(item) >= 0;
 		},
 		toggleActive(item) {
-			return this.getActive(item) ? this.isActive.splice(this.isActive.indexOf(item), 1) : this.isActive.push(item);
+			return this.isActive(item) ? this.actives.splice(this.actives.indexOf(item), 1) : this.actives.push(item);
 		},
 	},
 	init() {
 		fetch('resume.json')
 			.then(res => res.json())
-			.then(json => this.resume = json);
+			.then(json => {
+				this.resume = json;
+				this.filtered = {};
+
+				for(let section in this.resume) {
+					switch (section) {
+						case 'experience':
+						case 'education':
+						case 'volunteering':
+							// remove irrelevant entries
+							this.filtered[section] = this.resume[section].filter(item => item.relevant !== false);
+							break;
+						case 'about':
+							// flip order for columnal display
+							this.filtered[section] = [];
+							let half = Math.ceil(this.resume[section].length / 2);
+							for (let i = 0; i < this.resume[section].length; i++) {
+								let j = i % 2 === 0 ? i / 2 : half + Math.floor(i/2);
+								this.filtered[section][i] = this.resume[section][j];
+							}
+							break;
+						default:
+							// no filtering needed
+							this.filtered[section] = this.resume[section];
+							break;
+					}
+				}
+			});
 	},
 	template: `
-<section>
-	<header>
-		<img :src="resume.info.image" />
-		<h1>{{ resume.info.name }}</h1>
-		<p>{{ resume.info.description }}</p>
+<div>
+	<header class="flex-row flex-align-center">
+		<div class="flex-grow flex-shrink padding-right" style="flex-basis: 50%;">
+			<h1>{{ resume.info.name }}</h1>
+			<p>{{ resume.info.description }}</p>
+		</div>
+		<figure>
+			<img :src="resume.info.image" width="128" height="128" />
+		</figure>
 	</header>
 	<article class="about">
 		<header>
 			<h2>about</h2>
 		</header>
-		<ul>
-			<li v-for="skill in resume.skills">
-				<strong>{{ skill.name }}</strong>:
+		<ul class="flex-row">
+			<li v-for="skill of filtered.about" class="iconed">
+				<i class="fa fa-{{ skill.icon }}" :title="skill.name"></i>
 				<span>{{ skill.description + (skill.items ? topPicks(skill.items) + '.' : '') }}</span>
-				<a v-if="skill.items" href @click.prevent="toggleActive(skill)">Show {{ getActive(skill) ? 'Less' : 'More' }}</a>
-				<p v-if="skill.items" v-show="getActive(skill)">{{ listOfNames(skill.items) }}</p>
+				<button v-if="skill.items" @click="toggleActive(skill)" class="btn more"><i class="fa fa-{{ isActive(skill) ? 'minus' : 'plus' }}-circle"></i> {{ skill.items.length - 3 }} {{ isActive(skill) ? 'less' : 'more' }}</button>
+				<ul v-if="skill.items" v-show="isActive(skill)" class="more">
+					<li v-for="item of skill.items">{{ item.name }}<sup v-if="item.priority" class="color-primary">&star;</sup></li>
+				</ul>
 			</li>
 		</ul>
 	</article>
-	<article v-for="section of ['social','experience','education','volunteering']" :class="section">
+	<article class="social">
+		<header>
+			<h2>social</h2>
+		</header>
+		<ul class="flex-row flex-justify-around">
+			<li v-for="item of resume.social">
+				<a :href="item.url" :title="item.name" target="_blank" :class="'fa fa-' + className(item.name)"></a>
+			</li>
+		</ul>
+	</article>
+	<article v-for="section of ['experience','education','volunteering']">
 		<header>
 			<h2>{{ section }}</h2>
 		</header>
 		<ol>
-			<li v-for="item of resume[section]">
+			<li v-for="item of (isActive(section) ? resume : filtered)[section]">
 				<header>
-					<span v-if="item.title"><strong>{{ item.title }}</strong>,</span>
+					<strong v-if="item.title">{{ item.title }}</strong>
 					<a v-if="item.url" :href="item.url" target="_blank">{{ item.name }}</a>
 					<small v-if="item.duration">{{ item.duration }}</small>
 				</header>
 				<div v-if="item.description">{{ item.description }}</div>
 			</li>
 		</ol>
+		<footer>
+			<button v-if="filtered[section].length < resume[section].length" @click="toggleActive(section)" class="btn more"><i class="fa fa-{{ isActive(section) ? 'minus' : 'plus' }}-circle"></i> {{ resume[section].length - filtered[section].length }} {{ isActive(section) ? 'less' : 'more' }}</button>
+		</footer>
 	</article>
-</section>`,
+	<article>
+		<header>
+			<h2>references</h2>
+		</header>
+		<ul>
+			<li>
+				Available on <a :href="'mailto:' + resume.info.email" target="_blank">request</a>.
+			</li>
+		</ul>
+	</article>
+</div>`,
 });
 
 let MiApp = new Vue({
@@ -88,5 +158,8 @@ let MiApp = new Vue({
 	components: {
 		MiResume,
 	},
-	template: `<mi-resume></mi-resume>`,
+	template: `
+<main>
+	<mi-resume></mi-resume>
+</main>`,
 });
