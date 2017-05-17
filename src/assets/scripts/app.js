b39e8fcc6393e5c24f66db94c775c2dd75967e46
Murray Smith <msmith@smarttech.com>
2017-05-17 10:57:24 -0600

    add section linkability

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -5,7 +5,7 @@ let MiResume = Vue.extend({
 			filtered: null,
 			actives: [],
 			order: undefined,
-			url: location.href.replace(/[\?#].*$/, ''),
+			url: `https://mismith.io/resume/`,
 		};
 	},
 	methods: {
@@ -87,6 +87,15 @@ let MiResume = Vue.extend({
 							break;
 					}
 				}
+			})
+			.then(() => {
+				// auto-expand section (e.g. if landing from PDF link click)
+				setTimeout(() => {
+					let more = document.querySelector(`${location.hash} .btn.more`);
+					if (more) {
+						more.click();
+					}
+				}, 1000);
 			});
 	},
 	ready() {
@@ -112,13 +121,13 @@ let MiResume = Vue.extend({
 		<h2>{{ section }}</h2>
 	</header>
 	<ul class="flex-row">
-		<li v-for="article of filtered[section] | orderBy order" class="iconed">
+		<li v-for="(i, article) of filtered[section] | orderBy order" :id="section + '-' + i" class="iconed">
 			<i class="fa fa-{{ article.icon }}" :title="article.name"></i>
 			<span>{{ article.description }}</span>
 			<ol v-if="article.items" class="picks">
 				<li v-for="pick of topPicks(article.items)"><span v-if="!pick.url">{{ pick.name }}</span><a v-if="pick.url" :href="pick.url" target="_blank">{{ pick.name }}</a></li>
 			</ol>
-			<a v-if="article.items" :href="url" @click.prevent="toggleActive(article)" class="btn more"><i class="fa fa-{{ isActive(article) ? 'minus' : 'plus' }}-circle"></i> {{ article.items.length - 3 }}&nbsp;{{ isActive(article) ? 'less' : 'more' }}</a>
+			<a v-if="article.items" :href="url + '#' + section + '-' + i" @click.prevent="toggleActive(article)" class="btn more"><i class="fa fa-{{ isActive(article) ? 'minus' : 'plus' }}-circle"></i> {{ article.items.length - 3 }}&nbsp;{{ isActive(article) ? 'less' : 'more' }}</a>
 			<ul v-if="article.items" v-show="isActive(article)" class="more">
 				<li v-for="item of article.items | orderBy 'name'" :class="{priority: item.priority}">
 					<img v-if="item.url" :src="'https://www.google.com/s2/favicons?domain_url=' + item.url" height="12" />
@@ -157,7 +166,7 @@ let MiResume = Vue.extend({
 		</li>
 	</ol>
 	<footer>
-		<a v-if="filtered[section].length < resume[section].length" :href="url" @click.prevent="toggleActive(section)" class="btn more"><i class="fa fa-{{ isActive(section) ? 'minus' : 'plus' }}-circle"></i> {{ resume[section].length - filtered[section].length }}&nbsp;{{ isActive(section) ? 'less' : 'more' }}</a>
+		<a v-if="filtered[section].length < resume[section].length" :href="url + '#' + section" @click.prevent="toggleActive(section)" class="btn more"><i class="fa fa-{{ isActive(section) ? 'minus' : 'plus' }}-circle"></i> {{ resume[section].length - filtered[section].length }}&nbsp;{{ isActive(section) ? 'less' : 'more' }}</a>
 	</footer>
 </section>
 <section id="references">
